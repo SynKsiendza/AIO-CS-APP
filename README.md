adbed3b72613bc7d2cb4cd895577004b69f237ff
SynKsiendza <SynKsiendza@users.noreply.github.com>
2022-12-18 15:28:17 +0100

    Project Init [0.0.1 a001]

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# AIO-CS-APP
+# All in One App
+
+
+===
+## Name
+All in One APP
+
+---
+
+## Description
+This is Application, where I will put all of my projects and some of my coding knowledge.
+
+---
+## Installation
+Installation section at gitlab.com
+
+---
+## Used Tools
+### C# .NET 7.0 => https://dotnet.microsoft.com/en-us/
+### GitKraken   => https://www.gitkraken.com
+### VS Code     => https://code.visualstudio.com
+### GitLab      => https://gitlab.com
+
+---
+## Support
+For Support:
+    Contact at SynKsiendza@proton.me
+    Create an Issue at Gitlab.com
+---
+## Roadmap
+More info in CHANGELOG.md
+
+---
+## License
+More info at: "LICENSE" file
+
+---
+## Project status
+CURRENT STATUS: MAKING IN PROGRESS...
+---
---
+++ main/mathfunc.cs
@@ -0,0 +1,31 @@
+using System.Reflection.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathfunc
+{
+    public class ConverterFunction
+    {
+        public void conv()
+        {
+            
+        }
+        public ConverterFunction(int Input1, int Input2, int input3, int output1, string output2)
+        {
+            string[] NamesFreq={"MHz", "GHz", "Hz", "mHz"},
+            NamesCapacity={},
+            NamesITCap={},
+            NamesTerrain={},
+            NamesSpeed={},
+            NamesLong={},
+            NamesPower={},
+            NamesVoltage={},
+            NamesOther={};
+            
+            return(output1 + output2);
+        }
+    }
+}
